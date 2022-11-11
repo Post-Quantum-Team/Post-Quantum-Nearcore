fabc5539bfe3b0e59e4a2adb2fde299b06f0565c
LostAquilae <maximilienchaux@hotmail.fr>
2022-11-11 12:11:33 +0100

    Add a unit test for InMemorySigner structure in signer.rs

--- core/crypto/src/signer.rs
+++ core/crypto/src/signer.rs
@@ -44,7 +44,7 @@ impl Signer for EmptySigner {
 }
 
 /// Signer that keeps secret key in memory.
-#[derive(Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
 pub struct InMemorySigner {
     pub account_id: AccountId,
     pub public_key: PublicKey,
@@ -112,5 +112,51 @@ impl From<Arc<InMemorySigner>> for KeyFile {
             public_key: signer.public_key.clone(),
             secret_key: signer.secret_key.clone(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+    use serde_json::{from_str, to_string};
+    use std::str::FromStr;
+    use std::io::Write;
+
+    #[test]
+    fn test_in_memory_signer() {
+        fn load(contents: &[u8]) -> io::Result<InMemorySigner> {
+            let tmp = tempfile::NamedTempFile::new().unwrap();
+            tmp.as_file().write_all(contents).unwrap();
+            let result = InMemorySigner::from_file(tmp.path());
+            tmp.close().unwrap();
+            result
+        }
+
+        // Testing from seed
+        let account_id = AccountId::from_str("test").unwrap();
+        dbg!(&account_id);
+        let signer = InMemorySigner::from_seed(account_id, KeyType::ED25519, "test");
+        assert_eq!(signer.public_key, PublicKey::from_str("DcA2MzgpJbrUATQLLceocVckhhAqrkingax4oJ9kZ847").unwrap());
+        assert_eq!(signer.secret_key, SecretKey::from_str("3KyUuch8pYP47krBq4DosFEVBMR5wDTMQ8AThzM8kAEcBQEpsPdYTZ2FPX5ZnSoLrerjwg66hwwJaW1wHzprd5k3").unwrap());
+        assert_eq!(signer.account_id, AccountId::from_str("test").unwrap());
+        
+        // Testing from secret key
+        let secret_key = SecretKey::from_str("3KyUuch8pYP47krBq4DosFEVBMR5wDTMQ8AThzM8kAEcBQEpsPdYTZ2FPX5ZnSoLrerjwg66hwwJaW1wHzprd5k3").unwrap();
+        let account_id = AccountId::from_str("test").unwrap();
+        let signer = InMemorySigner::from_secret_key(account_id, secret_key);
+        assert_eq!(signer.public_key, PublicKey::from_str("DcA2MzgpJbrUATQLLceocVckhhAqrkingax4oJ9kZ847").unwrap());
+        assert_eq!(signer.secret_key, SecretKey::from_str("3KyUuch8pYP47krBq4DosFEVBMR5wDTMQ8AThzM8kAEcBQEpsPdYTZ2FPX5ZnSoLrerjwg66hwwJaW1wHzprd5k3").unwrap());
+        assert_eq!(signer.account_id, AccountId::from_str("test").unwrap());
+
+        //Testing from file
+        let signer = load(br#"{
+            "account_id": "test",
+            "public_key": "ed25519:DcA2MzgpJbrUATQLLceocVckhhAqrkingax4oJ9kZ847",
+            "secret_key": "ed25519:3KyUuch8pYP47krBq4DosFEVBMR5wDTMQ8AThzM8kAEcBQEpsPdYTZ2FPX5ZnSoLrerjwg66hwwJaW1wHzprd5k3"
+        }"#).unwrap();
+        assert_eq!(signer.public_key, PublicKey::from_str("DcA2MzgpJbrUATQLLceocVckhhAqrkingax4oJ9kZ847").unwrap());
+        assert_eq!(signer.secret_key, SecretKey::from_str("3KyUuch8pYP47krBq4DosFEVBMR5wDTMQ8AThzM8kAEcBQEpsPdYTZ2FPX5ZnSoLrerjwg66hwwJaW1wHzprd5k3").unwrap());
+        assert_eq!(signer.account_id, AccountId::from_str("test").unwrap());
     }
 }
